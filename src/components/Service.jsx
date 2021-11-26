44d94de8e5e28883df128c8d881651a9a8552c6c
Ryan (Chenguang) Xu <xcg1234@gmail.com>
2021-11-25 20:02:25 -0500

    Update Service.jsx

--- src/components/Service.jsx
+++ src/components/Service.jsx
@@ -1,25 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import Man from '../img/man.jpg';
 import Card from './Card';
 
+import Play from '../img/play.png';
+
 const Container = styled.div`
 	display: flex;
+	height: 100%;
 `;
 const Left = styled.div`
 	width: 50%;
+	position: relative;
 `;
 const Right = styled.div`
 	width: 50%;
 `;
 const Image = styled.img`
 	width: 100%;
+	display: ${(props) => props.open && 'none'};
 `;
 
 const Wrapper = styled.div`
 	padding: 50px;
 	display: flex;
 	flex-direction: column;
+`;
+
+const Video = styled.video`
+	display: ${(props) => !props.open && 'none'};
+	height: 300px;
+	position: absolute;
+	top: 0;
+	bottom: 0;
+	margin: auto;
+	right: 0;
 `;
 
 const Title = styled.h1``;
@@ -51,11 +66,25 @@ const Button = styled.button`
 	margin-left: 20px;
 `;
 
+const Icon = styled.img`
+	width: 20px;
+	margin-right: 10px;
+`;
+
 const Service = () => {
+	const [open, setOpen] = React.useState(false);
+
 	return (
 		<Container>
 			<Left>
-				<Image src={Man} />
+				<Image open={open} src={Man} />
+				<Video
+					autoPlay
+					loop
+					controls
+					open={open}
+					src='https://player.vimeo.com/external/449759244.sd.mp4?s=d5f3da46ddc17aa69a7de84f1e420610ebd2a391&profile_id=139&oauth2_token_id=57447761'
+				/>
 			</Left>
 			<Right>
 				<Wrapper>
@@ -72,7 +101,10 @@ const Service = () => {
 						<Card />
 						<Card />
 					</CardContainer>
-					<Button>How it works</Button>
+					<Button onClick={() => setOpen(true)}>
+						<Icon src={Play} />
+						How it works
+					</Button>
 				</Wrapper>
 			</Right>
 		</Container>
